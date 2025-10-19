1c7eb6a2f045ffbf5eff3c16b2c2205cc1fe13a5
Saltator <saltatorv@gmail.com>
2025-10-19 15:25:34 +0200

    [1][refactor] -> rename ParticlePointStyle to ParticlePointConfiguration

--- src/app/config/particle.config.ts
+++ src/app/config/particle.config.ts
@@ -1,11 +1,11 @@
-import {ParticlePointStyle} from '../shared/particle/particle-point-style';
+import {ParticlePointConfiguration} from '../shared/particle/particle-point-configuration';
 
 export enum ParticleConfigurationName {
   Default = 'Default',
   BackgroundCanvas = 'background-canvas'
 }
 
-export const PARTICLE_CONFIG: Record<ParticleConfigurationName, ParticlePointStyle> = {
+export const PARTICLE_CONFIG: Record<ParticleConfigurationName, ParticlePointConfiguration> = {
   [ParticleConfigurationName.Default]: {fillColor: '#0466c8', radius: 1},
   [ParticleConfigurationName.BackgroundCanvas]: {fillColor: '#0466c8', radius: 2}
 }
--- src/app/shared/particle-config-service.ts
+++ src/app/shared/particle-config-service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import {PARTICLE_CONFIG, ParticleConfigurationName} from '../config/particle.config';
-import {ParticlePointStyle} from './particle/particle-point-style';
+import {ParticlePointConfiguration} from './particle/particle-point-configuration';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ParticleConfigService {
-  getStyle(state: ParticleConfigurationName = ParticleConfigurationName.Default): ParticlePointStyle {
+  getStyle(state: ParticleConfigurationName = ParticleConfigurationName.Default): ParticlePointConfiguration {
     return PARTICLE_CONFIG[state];
   }
 }
